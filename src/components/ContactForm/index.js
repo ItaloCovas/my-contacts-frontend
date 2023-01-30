4928041190abb40ce1570ec205fe931b3cc5bd7e
Italo Covas <italocovas@gmail.com>
2023-01-30 16:56:53 -0300

    Form done

---
+++ src/components/ContactForm/index.js
@@ -0,0 +1,107 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import isEmailValid from '../../utils/isEmailValid';
+import formatPhone from '../../utils/formatPhone';
+import useErrors from '../../hooks/useErrors';
+
+import { Form, ButtonContainer } from './styles';
+
+import FormGroup from '../FormGroup';
+import Input from '../Input';
+import Select from '../Select';
+import Button from '../Button';
+
+export default function ContactForm({ buttonText }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+  const {
+    setError, removeError, getErrorMessageByFieldName, errors,
+  } = useErrors();
+
+  const isFormValid = (name && errors.length === 0);
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+
+    if (!e.target.value) {
+      setError({ field: 'name', message: 'Nome é obrigatório.' });
+    } else {
+      removeError('name');
+    }
+  }
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+
+    if (e.target.value && !isEmailValid(e.target.value)) {
+      setError({ field: 'email', message: 'E-mail inválido.' });
+    } else {
+      removeError('email');
+    }
+  }
+
+  function handlePhoneChange(e) {
+    setPhone(formatPhone(e.target.value));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <Form method="post" onSubmit={handleSubmit} noValidate>
+      <FormGroup error={getErrorMessageByFieldName('name')}>
+        <Input
+          placeholder="Nome (*)"
+          value={name}
+          onChange={handleNameChange}
+          error={getErrorMessageByFieldName('name')}
+        />
+      </FormGroup>
+
+      <FormGroup error={getErrorMessageByFieldName('email')}>
+        <Input
+          placeholder="E-mail"
+          type="email"
+          value={email}
+          onChange={handleEmailChange}
+          error={getErrorMessageByFieldName('email')}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={handlePhoneChange}
+          maxLength="15"
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="Instagram">Instagram</option>
+          <option value="Facebook">Facebook</option>
+          <option value="Github">Github</option>
+        </Select>
+      </FormGroup>
+
+      <ButtonContainer>
+        <Button type="submit" disabled={!isFormValid}>
+          {buttonText}
+        </Button>
+      </ButtonContainer>
+
+    </Form>
+  );
+}
+
+ContactForm.propTypes = {
+  buttonText: PropTypes.string.isRequired,
+};
